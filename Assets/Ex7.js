2d6a457790af65322133df24ef6e5e980e8e7f91
DovahkiinH <loick.caplier@gmail.com>
2023-09-07 10:37:50 +0200

    Ex7

---
+++ Assets/Ex7.js
@@ -0,0 +1,34 @@
+/* Script parité */
+
+var a = window.prompt("Saisissez un nombre");
+
+if (a%2 == 0) {
+
+    alert(a + " " + "est paire" );
+    
+}else 
+
+{
+
+    alert(a + " " + "est impaire");
+
+}
+
+/* Script pour calculer l'âge d'une personne */
+
+var b = window.prompt("Saisissez votre année de naissance");
+b = 2023 - b
+
+if (b >= 18) {
+
+    alert("vous avez" + " " + b + "ans" + " " + "vous etes donc majeur" );
+    
+}else 
+
+{
+
+    alert( "vous avez" + " " + b + "ans" + " " + "vous etes donc mineur" );
+
+}
+
+/* Script Calculatrice */
